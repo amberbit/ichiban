5f1462110999dd762fbc2ca0ea544da32d9f7f4e
Hubert <hubert.lepicki@amberbit.com>
2010-07-07 21:38:35 +0200

    index

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -3,6 +3,6 @@
 class Admin::UsersController < Admin::BaseController
   before_filter :authenticate_admin!
   def index
-    @users = User.find(:all, order: "email asc")
+    @users = User.find(:all, sort: "email asc")
   end
 end
