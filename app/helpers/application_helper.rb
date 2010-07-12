2238db1a7f61c68484987935971377201c0e480d
Hubert <hubert.lepicki@amberbit.com>
2010-07-12 16:39:59 +0200

    move helpers

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -8,4 +8,18 @@ module ApplicationHelper
       "<!-- Snippet #{name} was not found! -->".html_safe
     end
   end
+  def comment_form
+    div_populate('new_comment_wrapper', new_comment_path(path: request.path))
+  end
+
+  def comments_list
+    div_populate('comments_list_wrapper', comments_path(path: request.path))
+  end
+
+  def div_populate(id, path)
+    content_tag :div, :id => id, "data-populate-with" => path do
+      "<span class='loader'><span>Loading...</span></span>"
+    end.html_safe
+  end
+
 end
