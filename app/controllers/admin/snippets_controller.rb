a5261564397fcd9160519cc8b28fcae11f62a66e
Wojciech Piekutowski <wojciech@piekutowski.net>
2011-01-11 13:12:19 +0100

    Snippet search box added

--- app/controllers/admin/snippets_controller.rb
+++ app/controllers/admin/snippets_controller.rb
@@ -6,6 +6,15 @@ class Admin::SnippetsController < Admin::BaseController
 
   include Admin::GenericActions
 
+  def index
+    options = {sort: ["created_at", "desc"]}
+    unless params[:query].blank?
+      regexp = Regexp.new(params[:query], Regexp::IGNORECASE)
+      options[:conditions] = {content: regexp}
+    end
+    @resources = Snippet.all(options)
+  end
+
   protected
 
   def find_snippet;  @resource = Snippet.find params[:id]; end
